96f2199e78cab840b1fc1b933522107872762a0f
pdrVenancio <pedrosantos121229@gmail.com>
2024-02-09 19:35:43 -0300

    BD

---
+++ src/bancoDeDados.js
@@ -0,0 +1,25 @@
+// Funcao para criar os id
+const sequence = {
+    _id: 1,
+    get id(){return this._id++}
+}
+
+// Obj com os protutos
+const produtos = {}
+
+function salvaProduto(produto){
+    if(!produto.id) produto.id == sequence.id
+
+    produtos[produto[id]] = produto
+    return produto
+}
+
+function getProduto(id){
+    return produtos[id] || {}
+}
+
+function getProdutos(){
+    return Object.values(produtos)
+}
+
+module.exports = {salvaProduto, getProduto, getProdutos}
--- src/servidor.js
+++ src/servidor.js
@@ -1,12 +1,29 @@
 const porta = 3003
+
 const express = require('express')
 const app = express()
 
+const bancoDeDados =require('./bancoDeDados')
+
 app.get('/produtos', (req, res, next) => {
     //converter para json
-    res.send({nome: "Notebook", preco: 123.45})
+    res.send(bancoDeDados.getProdutos())
 })
 
+// Pesquisar produto por id
+app.get('/produtos/:id', (req, res, next) => {
+    // req.params. para acessar os parametros( :id, ...)
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+// Salvar produtos
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvaProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
 })
